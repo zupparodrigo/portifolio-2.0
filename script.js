3cb21d898aa12881b9d7b5c5a4cdd45258a4bb22
rodrigo santos zuppa <rodrigozuppa0@gmail.com>
2024-09-17 19:31:50 +0000

    ultimo commit

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function atualizarPreco(){
 
     let preco = qtde * 100;
     if (temJS) preco *= 1.1
-    if (incluiLayout) preco += 500
+    if (incluiLayout) preco += 450
     let taxaUrgencia = 1 - prazo*0.1;
     preco *= 1 + taxaUrgencia
 
@@ -55,13 +55,13 @@ function debounce(func, wait, immediate) {
 
 
 
-// animação scrool =============================
+
 const target = document.querySelectorAll("[data-anime]")
 
 const animationClass = "animate"
 
 function animeScroll() {
-    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4)
+    const windowTop = window.pageYOffset + ((window.innerHeight * 2) / 4)
     target.forEach(function(element) {
         if((windowTop) > element.offsetTop){
             element.classList.add(animationClass)
@@ -75,7 +75,7 @@ function animeScroll() {
 if(target.length) {
     window.addEventListener('scroll', debounce(function() {
         animeScroll()
-    },10))
+    },7))
 }
 
 
